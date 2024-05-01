2c6e22b1154221b9a2af156b7379b62f2359780a
JohnnyandLee <adm801209@gmail.com>
2024-05-01 11:01:46 -0700

    revise README and final check

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,23 +1,25 @@
 import './index.css'
-import { useRef } from "react"
+import { useState, useRef } from "react"
 import postData from '../../global/postData'
 
 const Search = ({searchNewData, spinChange, spin, notArtistChange, fillArtistBio, fillArtwork, chooseLeft}) => {
     const inputName = useRef("");
+    const [inputValue, setInputValue] = useState("");
 
     let handleKeyPress = (event) => {
         if (event.key === 'Enter') {
-          searchName();
+            searchName();
         }
     };
 
     let searchName = () => {
        const name = inputName.current.value;
        if(name === "") return;
-       inputName.current.value = "";
 
        //clear the previous material
        clearAll();
+       inputName.current.value = name;
+       setInputValue(name);
 
        spinChange(true);
 
@@ -40,6 +42,8 @@ const Search = ({searchNewData, spinChange, spin, notArtistChange, fillArtistBio
     }
 
     let clearAll = () => {
+        inputName.current.value = "";
+        setInputValue("");
         searchNewData([]);
         fillArtistBio({});
         fillArtwork([]);
@@ -55,13 +59,18 @@ const Search = ({searchNewData, spinChange, spin, notArtistChange, fillArtistBio
             <div className="container">
                 <div className="row">
                     <div className="col-md-12">
-                        <input type="text" ref={inputName} className="form-control" onKeyDown={handleKeyPress} placeholder="Please enter an artist name." />
+                        <input type="text" 
+                               ref={inputName} 
+                               className="form-control" 
+                               onKeyDown={handleKeyPress} 
+                               onChange={(e) => setInputValue(e.target.value)}
+                               placeholder="Please enter an artist name." />
                     </div>
                 </div>
 
                 <div className="row mt-3">
                     <div className="col-md-12 text-center">
-                        <button onClick={searchName} className="btn" id="inputsend" type="submit">
+                        <button onClick={searchName} className="btn" id="inputsend" type="submit" disabled={inputValue === ""}>
                             Search
                             {spin && <div className="spinner-border spinner-border-sm text-light"></div>}
                         </button>
